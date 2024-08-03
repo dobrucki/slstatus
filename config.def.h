59a8d497b2db355ba1ad66241ae8a5178bd7ced0
dobrucki <dobrucki2@gmail.com>
2024-08-03 20:42:13 +0200

    feat: configure status bar

--- config.def.h
+++ config.def.h
@@ -66,7 +66,7 @@ static const char unknown_str[] = "n/a";
 static const struct arg args[] = {
 	/* function  format          argument */
 	{ cpu_perc,  "| CPU %s% |",  NULL    },
-	{ ram_used,  " %s",          NULL    },
+	{ ram_used,  " MEM %s",      NULL    },
 	{ ram_total, " / %s |",      NULL    },
 	{ datetime,  " %s |",        "%F %T" },
 };
