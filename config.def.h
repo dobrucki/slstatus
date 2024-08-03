aa40e16f8748b4653eeb837af31259bae282fc4f
dobrucki <dobrucki2@gmail.com>
2024-08-03 20:37:13 +0200

    feat: configure status bar

--- config.def.h
+++ config.def.h
@@ -64,6 +64,9 @@ static const char unknown_str[] = "n/a";
  * wifi_perc           WiFi signal in percent          interface name (wlan0)
  */
 static const struct arg args[] = {
-	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
+	/* function  format          argument */
+	{ cpu_perc,  "| CPU %s% |",  NULL    },
+	{ ram_used,  " %s",          NULL    },
+	{ ram_total, " / %s |",      NULL    },
+	{ datetime,  " %s |",        "%F %T" },
 };
